16061e022a412a0b175ba856fcc6421894e98339
Mkarimifar_utsMacbook <em.karimifar@gmail.com>
2021-06-25 00:22:01 -0500

    ex2

--- ex2/assets/js/app.js
+++ ex2/assets/js/app.js
@@ -8,11 +8,54 @@ var outTime = 4000;
 var intervalColor;
 var outColor;
 var delay;
-var rocksN = 16;
+var spinnerN = 8;
 
+for (var i =1; i<=spinnerN; i++){
+    if(i==1){
+        var select = 'display'
+    }else{
+        var select = null;
+    }
+    var img = '<img src="./assets/img/spinners/Spinner_'+i+'.png">';
+    var navItem = $('<div class="spinner-nav-item spinner_'+i+' ' +select+'" data-spinner='+i+'>').append(img);
+    
+    $('#spinner-nav').append(navItem)
+}
 
+function updateTime(val) {
+    $('#textInput').value=val; 
+}
+$('#timeRange').on('input', function() {
+    var time = $(this).val()
+    $('#textInput').html( time+' ms' );
+    changeTime(time)
+});
 
+function changeTime(time){
+    $('#spinner').css('animation-duration',time+'ms')
+}
 
+$('#spinner').on('click',function(){
+    var el = document.getElementById('spinner');
+    if($("#spinner").hasClass('stopped')){
+        $("#spinner").css('animation-name','spin')
+        $("#spinner").removeClass("stopped")
+    }else{
+        var st = window.getComputedStyle(el, null);
+        var tr = st.getPropertyValue("-webkit-transform") ||
+        st.getPropertyValue("-moz-transform") ||
+        st.getPropertyValue("-ms-transform") ||
+        st.getPropertyValue("-o-transform") ||
+        st.getPropertyValue("transform") ||
+        "fail...";
+        
+        $("#spinner").css('animation-name','none')
+        $("#spinner").css('transform',tr)
+        $("#spinner").addClass('stopped')
+    }
+    // console.log(tr)
+
+})
 // function resetSizes(){
 //     var squareSize;
 //     if(h>=w){
@@ -27,7 +70,7 @@ var rocksN = 16;
 window.onresize = function(event) {
     w = window.innerWidth;
     h = window.innerHeight;
-    resetSizes();
+    // resetSizes();
     console.log("resized")
 };
 
@@ -37,7 +80,14 @@ $('#tools-handle').on('click',function(){
 })
 
 
+$('.spinner-nav-item').on('click', function(){
+    var spinner = 'spinner_' + $(this).data('spinner');
+    var img = './assets/img/spinners/'+spinner+'.png';
+    $('#spinner').attr('src',img);
+    $('.spinner-nav-item').removeClass('display');
+    $('.spinner-nav-item').css('border-bottom-color','#ffffff00')
+    $('.'+spinner).addClass('display')
+    $('.spinner-nav-item.display').css('border-bottom-color','#222')
+})
 
 
-colorShift();
-resetSizes();
