37e73036148c9ee0529c612dc8d310e8e003e7b1
Mkarimifar_utsMacbook <em.karimifar@gmail.com>
2021-08-31 17:39:05 -0500

    card game initial

---
+++ covers-crosses-crowns/assets/js/app.js
@@ -0,0 +1,134 @@
+var w = window.innerWidth;
+var h = window.innerHeight;
+window.onresize = function(event) {
+    w = window.innerWidth;
+    h = window.innerHeight;
+    cardH = cardW*w/h*1.4;
+    updatePositions();
+    // resetSizes();
+};
+
+$('#tools-handle').on('click',function(){
+    $('#toolbar').toggleClass('closed')
+})
+var cardW = 8;
+var cardH = cardW*w/h*1.4;
+var totalMargin = 100 - (5 * cardW);
+var marginUnit = totalMargin/15;
+var cards = {
+    design: [1,2,3,4,5,6,7,8],
+    colors: ['B', 'C', 'G', 'M', 'R', 'Y']
+}
+var board = [
+    {
+        num: 0,
+        pos: [4.5*marginUnit,5]
+    },
+    {
+        num: 1,
+        pos: [(7.5*marginUnit)+2*cardW, 3*marginUnit + cardH]
+    },
+    {
+        num: 2,
+        pos: [(7.5*marginUnit)+2*cardW, 3*marginUnit + cardH]
+    },
+    {
+        num: 3,
+        pos: [(7.5*marginUnit)+2*cardW, 4*marginUnit + 2*cardH]
+    },
+    {
+        num: 4,
+        pos: [6.5*marginUnit + cardW, 3*marginUnit + cardH]
+    },
+    {
+        num: 5,
+        pos: [(7.5*marginUnit)+2*cardW, 2*marginUnit]
+    },
+    {
+        num: 6,
+        pos: [(8.5*marginUnit)+3*cardW, 3*marginUnit + cardH]
+    },
+    {
+        num: 7,
+        pos: [(10.5*marginUnit)+4*cardW, 4*marginUnit/2 + 3*cardH]
+    },
+    {
+        num: 8,
+        pos: [(10.5*marginUnit)+4*cardW, 3*marginUnit/2 + 2*cardH]
+    },
+    {
+        num: 9,
+        pos: [(10.5*marginUnit)+4*cardW, 2*marginUnit/2 + 1*cardH]
+    },
+    {
+        num: 10,
+        pos: [(10.5*marginUnit)+4*cardW, marginUnit/2]
+    },
+]
+
+
+for(var i =0; i<board.length; i++){
+    var card_pos = $('<div class="card-pos" id="card-pos-'+i+'">')
+    var card_wrap = $('<div class="card-wrap" id="card-wrap-'+i+'">')
+    card_pos.css('left', board[i].pos[0]+'%')
+    card_pos.css('top', board[i].pos[1]+'%')
+    card_wrap.append('<p>'+i+'</p>')
+    card_pos.append(card_wrap)
+    $('#board').append(card_pos)
+}
+
+
+function updatePositions(){
+    board = [
+        {
+            num: 0,
+            pos: [4.5*marginUnit,5]
+        },
+        {
+            num: 1,
+            pos: [(7.5*marginUnit)+2*cardW, 3*marginUnit + cardH]
+        },
+        {
+            num: 2,
+            pos: [(7.5*marginUnit)+2*cardW, 3*marginUnit + cardH]
+        },
+        {
+            num: 3,
+            pos: [(7.5*marginUnit)+2*cardW, 4*marginUnit + 2*cardH]
+        },
+        {
+            num: 4,
+            pos: [6.5*marginUnit + cardW, 3*marginUnit + cardH]
+        },
+        {
+            num: 5,
+            pos: [(7.5*marginUnit)+2*cardW, 2*marginUnit]
+        },
+        {
+            num: 6,
+            pos: [(8.5*marginUnit)+3*cardW, 3*marginUnit + cardH]
+        },
+        {
+            num: 7,
+            pos: [(10.5*marginUnit)+4*cardW, 4*marginUnit/2 + 3*cardH]
+        },
+        {
+            num: 8,
+            pos: [(10.5*marginUnit)+4*cardW, 3*marginUnit/2 + 2*cardH]
+        },
+        {
+            num: 9,
+            pos: [(10.5*marginUnit)+4*cardW, 2*marginUnit/2 + 1*cardH]
+        },
+        {
+            num: 10,
+            pos: [(10.5*marginUnit)+4*cardW, marginUnit/2]
+        },
+    ]
+    for(var i =0; i<board.length; i++){
+        console.log(board[i].pos[1])
+        selector = '#card-pos-'+i;
+        $(selector).css('left', board[i].pos[0]+'%')
+        $(selector).css('top', board[i].pos[1]+'%')
+    }
+}
