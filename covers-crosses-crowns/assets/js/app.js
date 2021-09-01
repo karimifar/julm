9a7c710fa3dbcc2ff49517f0f2591437ffbb346d
Mkarimifar_utsMacbook <em.karimifar@gmail.com>
2021-09-01 00:20:46 -0500

    shuffling deck and putting on board

--- covers-crosses-crowns/assets/js/app.js
+++ covers-crosses-crowns/assets/js/app.js
@@ -17,12 +17,14 @@ var totalMargin = 100 - (5 * cardW);
 var marginUnit = totalMargin/15;
 var cards = {
     design: [1,2,3,4,5,6,7,8],
-    colors: ['B', 'C', 'G', 'M', 'R', 'Y']
+    colors: ['B', 'C', 'G', 'M', 'R', 'Y'],
+    images: []
 }
 var board = [
     {
         num: 0,
-        pos: [4.5*marginUnit,5]
+        //3 was 4.5 before
+        pos: [3*marginUnit,5]
     },
     {
         num: 1,
@@ -78,11 +80,39 @@ for(var i =0; i<board.length; i++){
 }
 
 
+for (var i=0; i<cards.design.length; i++){
+    // var img = 
+    for(var j=0; j<cards.colors.length; j++){
+        var img = 'card_'+cards.design[i] + '_'+cards.colors[j]
+        cards.images.push(img)
+    }
+}
+console.log(cards.images)
+shuffleCards(cards.images)
+
+function shuffleCards(imgArr){
+    for (var i = imgArr.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = imgArr[i];
+        imgArr[i] = imgArr[j];
+        imgArr[j] = temp;
+    }
+    for(var q=0; q<imgArr.length;q++){
+        var imgUrl = './assets/img/cards/'+imgArr[q]+'.png'
+        var img = $('<img src="'+imgUrl+'">' )
+        $('#card-wrap-0').append(img)
+        img.css('left', q/2+'px')
+        // img.css('top', q/2+'px')
+    }
+
+}
+
 function updatePositions(){
     board = [
         {
             num: 0,
-            pos: [4.5*marginUnit,5]
+            //3 was 4.5 before
+            pos: [3*marginUnit,5]
         },
         {
             num: 1,
