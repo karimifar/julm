a38d35e786a59349face7e9ea420a1c3e578250a
Mkarimifar_utsMacbook <em.karimifar@gmail.com>
2021-09-06 00:02:49 -0500

    first lecture page

---
+++ lectures/assets/js/lectures.js
@@ -0,0 +1,17 @@
+$('#dark-switch').on('click', function(){
+    console.log('switch')
+    $('p, body').toggleClass('dark')
+})
+
+var gridH = 15
+var gridV = 20
+
+function createGrid(h,v){
+    for(var i=0; i<h; i++){
+        $('#grid-h-wrap').append('<div class="grid-h"></div>')
+    }
+    for(var i=0; i<v; i++){
+        $('#grid-v-wrap').append('<div class="grid-v"></div>')
+    }
+}
+createGrid(gridH,gridV)
